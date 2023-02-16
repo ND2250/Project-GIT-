6ab7793fcf5b917bf0cff6840086927a400b2031
Nishant Dhanani <nishant.dhanani@gmail.com>
2023-02-16 22:27:36 +0000

    GIT HUB

---
+++ workhard/Taskscreated.java
@@ -0,0 +1,12 @@
+package workhard;
+
+public class Taskscreated {
+    public static void main(String[] args){
+        System.out.println("Waterfall");
+        System.out.println("Sprint");
+        System.out.println("Not in one");
+        
+
+
+    }
+}
